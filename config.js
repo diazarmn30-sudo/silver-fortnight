d1e0f33f1a60902ba6f14acc33144379c9ec7486
diazarmn30-sudo <diazarmn30@gmail.com>
2026-01-13 21:52:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     
     ownerId: 5966985421, // ID LU
     
-    telegramBotToken: "8556755989:AAGP_v6A8dWIkqtOede0fUcVdJpzBWkdS1w",
+    telegramBotToken: "8328012642:AAFh-d1fPCYldgRjdoa7NqpFGXph9rUG1rc",
     sessionName: "session",
 
 
