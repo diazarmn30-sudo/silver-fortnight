f76345acf16645391649f464bb0aaeedef449752
diazarmn30-sudo <diazarmn30@gmail.com>
2026-01-14 10:18:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,18 @@
 module.exports = {
-    
-    ownerId: 5966985421, // ID LU
-    
-    telegramBotToken: "8328012642:AAGPm-vshpuTet0mEzA9pVUFtCVxPT7_tWA",
-    sessionName: "session",
+  telegramBotToken: "8328012642:AAGPm-vshpuTet0mEzA9pVUFtCVxPT7_tWA",
+  ownerId: 5966985421,          // ID telegram kamu (angka)
+  sessionName: "session",
 
+  settings: {
+    cekBioBatchSize: 50,
+    cekBioDelayMs: 50,         // kamu mau 100 ya tinggal ganti 100
+  },
 
-    photoStart: "https://files.catbox.moe/mqf20e.jpg",
+  message: {
+    premium: "💎 Khusus member premium, bos!",
+    owner: "🚫 Khusus owner, bos!",
+    waNotConnected: "⚠️ WA belum nyambung. /pairing dulu.",
+  },
 
-    message: {
-        owner: `🚫 Cuma yaz yang bisa, bos!`,
-        premium: `💎 Khusus member premium, bos!`,
-        wait: `⏳ Otw yaz, sabar ya...`,
-        error: `⚠️ Anjir, error yaz. Coba lagi ntar.`,
-        waNotConnected: `⚠️ Waduh, WA-nya belum nyambung yaz. /pairing dulu lah.`
-    },
-
-    settings: {
-        namabot: "Cek Bio By yaz",
-        footer: "Powered By yaz",
-        cekBioBatchSize: 30 // Jumlah nomor yang dicek bersamaan.
-    }
+  photoStart: "" // optional
 };
